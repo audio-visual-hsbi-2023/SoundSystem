2a7fa14c26ccf2de1c486f43c90302c4b484099c
mbl.85 <mbl.85@web.de>
2024-01-15 16:11:44 +0100

    Added more phases

--- src/conf/Config.py
+++ src/conf/Config.py
@@ -11,5 +11,25 @@ class Config:
             order=1,
             name="Phase A - Intro",
             directory="phase_A_intro"
+        ),
+        Phase(
+            order=2,
+            name="Phase B - Buildup",
+            directory="phase_B_buildup"
+        ),
+        Phase(
+            order=3,
+            name="Phase C - Peak",
+            directory="phase_C_peak"
+        ),
+        Phase(
+            order=4,
+            name="Phase D - Descent",
+            directory="phase_D_descent"
+        ),
+        Phase(
+            order=5,
+            name="Phase E - Welcome back",
+            directory="phase_E_welcomeback"
         )
     ]
